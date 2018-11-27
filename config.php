db3d276073e77b0f049b47b9d99836ccb11f852e
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-27 11:44:47 +0100

    Added session type factories to config

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
 
-return [];
+return [
+    'eddbk_session_generator' => [
+        'session_type_factories' => [
+            'fixed_duration' => 'eddbk_fixed_duration_session_type_factory'
+        ]
+    ]
+];
