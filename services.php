d7e689d4eac6dcd16213ccc40a51996dda731ccc
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-27 11:46:48 +0100

    Updated services

--- services.php
+++ services.php
@@ -1,35 +1,51 @@
 <?php
 
-use Dhii\Factory\GenericCallbackFactory;
 use Psr\Container\ContainerInterface;
+use RebelCode\Bookings\Sessions\SessionGenerator;
+use RebelCode\EddBookings\Sessions\Generator\FixedDurationSessionTypeFactory;
+use RebelCode\EddBookings\Sessions\Generator\SessionTypeFactory;
+use RebelCode\EddBookings\Sessions\Module\AvailabilityFactory;
 use RebelCode\EddBookings\Sessions\Module\GenerateSessionsHandler;
-use RebelCode\EddBookings\Sessions\Module\SessionRuleFactory;
 use RebelCode\EddBookings\Sessions\Time\PeriodFactory;
-use RebelCode\Sessions\SessionGenerator;
 
 return [
-    'eddbk_session_generator_factory' => function (ContainerInterface $c) {
-        return new GenericCallbackFactory(function ($config) {
-            $sessionFactory = $this->_containerGet($config, 'session_factory');
-            $sessionLengths = $this->_containerGet($config, 'session_lengths');
-
-            return new SessionGenerator($sessionFactory, $sessionLengths);
-        });
+    'eddbk_session_generator' => function (ContainerInterface $c) {
+        return new SessionGenerator();
     },
+
     'eddbk_generate_sessions_handler' => function (ContainerInterface $c) {
         return new GenerateSessionsHandler(
-            $c->get('eddbk_session_generator_factory'),
-            $c->get('eddbk_session_rule_factory'),
-            $c->get('session_rules_select_rm'),
+            $c->get('eddbk_session_generator'),
+            $c->get('eddbk_services_manager'),
+            $c->get('resources_entity_manager'),
+            $c->get('eddbk_session_type_factory'),
+            $c->get('eddbk_availability_factory'),
             $c->get('sessions_insert_rm'),
             $c->get('sessions_delete_rm'),
             $c->get('sql_expression_builder')
         );
     },
-    'eddbk_session_rule_factory'      => function (ContainerInterface $c) {
-        return new SessionRuleFactory($c->get('eddbk_period_factory'));
+
+    'eddbk_availability_factory' => function (ContainerInterface $c) {
+        return new AvailabilityFactory();
     },
-    'eddbk_period_factory'            => function (ContainerInterface $c) {
+
+    'eddbk_session_type_factory' => function (ContainerInterface $c) {
+        $factoriesCfg = $c->get('eddbk_session_generator/session_type_factories');
+
+        $factories = [];
+        foreach ($factoriesCfg as $_type => $_service) {
+            $factories[$_type] = $c->get($_service);
+        }
+
+        return new SessionTypeFactory($factories);
+    },
+
+    'eddbk_fixed_duration_session_type_factory' => function (ContainerInterface $c) {
+        return new FixedDurationSessionTypeFactory();
+    },
+
+    'eddbk_period_factory' => function (ContainerInterface $c) {
         return new PeriodFactory();
     },
 ];
